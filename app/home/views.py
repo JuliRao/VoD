324fe1fe6e009b52c25ee7ea53bfd01ff93fe49d
rm031 <marao2048@gmail.com>
2019-05-19 21:28:14 +0800

    add basic UIs

--- app/home/views.py
+++ app/home/views.py
@@ -13,11 +13,7 @@ def animation():
     return render_template("home/animation.html")
 
 
-@home.route("/search/")
-def search():
-    return render_template("home/search.html")
-
-
 @home.route("/play/")
 def play():
     return render_template("home/play.html")
+
